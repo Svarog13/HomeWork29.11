092bc428986819e834ecd4f9f29c2e638ccb56d8
Void <Void@SWEETVOID1488>
2023-11-29 22:16:16 +0200

    Add project files.

---
+++ Ex2/Program.cs
@@ -0,0 +1,50 @@
+﻿using System;
+using System.Xml.Linq;
+
+class Program
+{
+    static void Main(string[] args)
+    {
+        using (Shop myShop = new Shop("SuperMart", "123 Main Street", ShopType.Grocery))
+        {
+            myShop.ShowInfo();
+        }
+    }
+    enum ShopType
+    {
+        Grocery,
+        Household,
+        Clothing,
+        Footwear
+    }
+
+    class Shop : IDisposable
+    {
+        public string NameOfShop { get; set; }
+        public string Adress { get; set; }
+        public ShopType Type { get; set; }
+
+        public Shop(string nameOfShop, string adress, ShopType type)
+        {
+            NameOfShop = nameOfShop;
+            Adress = adress;
+            Type = type;
+        }
+        public void ShowInfo()
+        {
+            Console.WriteLine($"Shop Name: {NameOfShop}");
+            Console.WriteLine($"Adress: {Adress}");
+            Console.WriteLine($"Type: {Type}");
+        }
+        public void Dispose()
+        {
+            Console.WriteLine($"Shop{NameOfShop} is being disposed.");
+
+        }
+        ~Shop()
+        {
+            Console.WriteLine($"Destructor called for Shop {NameOfShop}");
+        }
+    }
+}
+
---
+++ HomeWork29.11/Program.cs
@@ -0,0 +1,44 @@
+﻿using System;
+
+class Program
+{
+    static void Main(string[] args)
+    {
+        Piece myPiece = new Piece("Moonlight Sonata", "Ludwig van Beethoven", "Classical", 1801);
+        myPiece.ShowInfo();
+    }
+
+    class Piece:IDisposable
+    {
+        public string Title { get; set; }
+        public string NameOfAuthor { get; set; }
+        public string Genre { get; set; }
+        public int Year { get; set; }
+
+        public Piece(string title, string nameOfAuthor, string genre, int year)
+        {
+            Title = title;
+            NameOfAuthor = nameOfAuthor;
+            Genre = genre;
+            Year = year;
+        }
+
+        public void ShowInfo()
+        {
+            Console.WriteLine($"Title: {Title}");
+            Console.WriteLine($"NameOfAuthor: {NameOfAuthor}");
+            Console.WriteLine($"Genre: {Genre}");
+            Console.WriteLine($"Year: {Year}");
+        }
+        public void Dispose()
+        {
+            Console.WriteLine($"Shop{Title} is being disposed.");
+
+        }
+
+        ~Piece()
+        {
+            Console.WriteLine($"Destructor called for title {Title}");
+        }
+    }
+}
